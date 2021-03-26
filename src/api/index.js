371fc99da32966c8c633e40268b224bc2089ca75
sun.lf <sunlifei@126.com>
2021-03-26 22:28:41 +0800

    1

--- src/api/index.js
+++ src/api/index.js
@@ -1,16 +1,16 @@
 import request from '../../utils/request'
 export function syncFunc(data) {
     return request({
-      //  url: 'https://shielded-oasis-36680.herokuapp.com/dbdata',
-        url: 'http://localhost:3000/dbdata',
+        url: 'https://shielded-oasis-36680.herokuapp.com/dbdata',
+       // url: 'http://localhost:3000/dbdata',
         method: 'get',
         data: data || {},
     })
 }
 export function addSite(data) {
     return request({
-       // url: 'https://shielded-oasis-36680.herokuapp.com/add',
-        url: 'http://localhost:3000/add',
+        url: 'https://shielded-oasis-36680.herokuapp.com/add',
+        //url: 'http://localhost:3000/add',
         method: 'POST',
         data: data || {},
     })
